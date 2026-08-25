534f5911f371aa35ef1e4ba0fc3829d419a1f51f
Marcus Becker <75479673+MarcusBecker-GitHub@users.noreply.github.com>
2026-08-25 14:12:45 +0200

    Adapted turbine controller class to multiple turbines

--- src/off/TurbineModel/TurbineController/TurbineController.py
+++ src/off/TurbineModel/TurbineController/TurbineController.py
@@ -6,7 +6,9 @@ import numpy as np
 from off.OFFModule import *
 
 class TurbineController(OFFModule):
-    """Base interface for local (per-turbine) controller."""
+    """Base interface for local turbine controller.
+    Has to potentially cover multiple turbines of one type controller in the same manner but with individual setpoints.
+    """
     
     MODULE_TYPE = "TurbineController"
     
@@ -17,143 +19,152 @@ class TurbineController(OFFModule):
     """
 
     @compatibility(CompatibilityLevel.NONE)    
-    def obs_power_setpoint_w(self, t_s: np.float64) -> np.float64:
+    def obs_power_setpoint_w(self, t_s: np.float64, t_ids: np.ndarray) -> np.ndarray:
         """ Abstract method to observe the current power setpoint of the turbine.
 
         Args:
             t_s (np.float64): Current simulation time in seconds.
+            t_ids (np.ndarray): Array of turbine IDs.
 
         Raises:
             NotImplementedError: Abstract Method, must be implemented in derived classes.
 
         Returns:
-            np.float64: Current power setpoint of the turbine (W).
+            np.ndarray: Current power setpoint(s) of the turbine(s) (W).
         """
         raise NotImplementedError
     
     @abstractmethod
     @compatibility(CompatibilityLevel.NONE)
-    def obs_yaw_setpoint_deg(self, t_s: np.float64) -> np.float64:
-        """ Observes the current yaw setpoint of the turbine.
+    def obs_yaw_setpoint_deg(self, t_s: np.float64, t_ids: np.ndarray) -> np.ndarray:
+        """ Observes the current yaw misalignment setpoint of the turbine(s).
 
         Args:
             t_s (np.float64): Current simulation time in seconds.
+            t_ids (np.ndarray): Array of turbine IDs.
 
         Raises:
             NotImplementedError: Abstract Method, must be implemented in derived classes.
 
         Returns:
-            np.float64: Current yaw setpoint of the turbine (deg).
+            np.ndarray: Current yaw setpoint(s) of the turbine(s) (deg).
         """
         raise NotImplementedError
     
     @compatibility(CompatibilityLevel.NONE)   
-    def obs_collective_pitch_setpoint_deg(self, t_s: np.float64) -> np.float64:
+    def obs_collective_pitch_setpoint_deg(self, t_s: np.float64, t_ids: np.ndarray) -> np.ndarray:
         """ Observes the current collective pitch setpoint of the turbine.
 
         Args:
             t_s (np.float64): Current simulation time in seconds.
+            t_ids (np.ndarray): Array of turbine IDs.
 
         Raises:
             NotImplementedError: Abstract Method, must be implemented in derived classes.
 
         Returns:
-            np.float64: Current collective pitch setpoint of the turbine (deg).
+            np.ndarray: Current collective pitch setpoint(s) of the turbine(s) (deg).
         """
         raise NotImplementedError
     
     @compatibility(CompatibilityLevel.NONE)   
-    def obs_individual_pitch_setpoints_deg(self, t_s: np.float64) -> np.ndarray:
+    def obs_individual_pitch_setpoints_deg(self, t_s: np.float64, t_ids: np.ndarray) -> np.ndarray:
         """ Observes the current individual pitch setpoints of the turbine.
 
         Args:
             t_s (np.float64): Current simulation time in seconds.
+            t_ids (np.ndarray): Array of turbine IDs.
 
         Raises:
             NotImplementedError: Abstract Method, must be implemented in derived classes.
 
         Returns:
-            np.ndarray: Current individual pitch setpoints of the turbine (deg).
+            np.ndarray: Current individual pitch setpoints of the turbine(s) (deg) with shape (n_blades, n_turbines).
         """
         raise NotImplementedError
     
     @compatibility(CompatibilityLevel.NONE)   
-    def obs_generator_torque_setpoint_nm(self, t_s: np.float64) -> np.float64:
+    def obs_generator_torque_setpoint_nm(self, t_s: np.float64, t_ids: np.ndarray) -> np.ndarray:
         """ Observes the current generator torque setpoint of the turbine.
 
         Args:
             t_s (np.float64): Current simulation time in seconds.
+            t_ids (np.ndarray): Array of turbine IDs.
 
         Raises:
             NotImplementedError: Abstract Method, must be implemented in derived classes.
 
         Returns:
-            np.float64: Current generator torque setpoint of the turbine (Nm).
+            np.ndarray: Current generator torque setpoint(s) of the turbine(s) (Nm).
         """
         raise NotImplementedError
     
     @compatibility(CompatibilityLevel.NONE)   
-    def obs_rotor_torque_setpoint_nm(self, t_s: np.float64) -> np.float64:
+    def obs_rotor_torque_setpoint_nm(self, t_s: np.float64, t_ids: np.ndarray) -> np.ndarray:
         """ Observes the current rotor torque setpoint of the turbine.
 
         Args:
             t_s (np.float64): Current simulation time in seconds.
+            t_ids (np.ndarray): Array of turbine IDs.
 
         Raises:
             NotImplementedError: Abstract Method, must be implemented in derived classes.
 
         Returns:
-            np.float64: Current rotor torque setpoint of the turbine (Nm).
+            np.ndarray: Current rotor torque setpoint(s) of the turbine(s) (Nm).
         """
         raise NotImplementedError
     
     @compatibility(CompatibilityLevel.NONE)   
-    def obs_rotor_speed_setpoint_radps(self, t_s: np.float64) -> np.float64:
+    def obs_rotor_speed_setpoint_radps(self, t_s: np.float64, t_ids: np.ndarray) -> np.ndarray:
         """ Observes the current rotor speed setpoint of the turbine.
 
         Args:
             t_s (np.float64): Current simulation time in seconds.
+            t_ids (np.ndarray): Array of turbine IDs.
 
         Raises:
             NotImplementedError: Abstract Method, must be implemented in derived classes.
 
         Returns:
-            np.float64: Current rotor speed setpoint of the turbine (rad/s).
+            np.ndarray: Current rotor speed setpoint(s) of the turbine(s) (rad/s).
         """
         raise NotImplementedError
     
     @compatibility(CompatibilityLevel.OPTIONAL)   
-    def obs_rotor_speed_setpoint_rpm(self, t_s: np.float64) -> np.float64:
+    def obs_rotor_speed_setpoint_rpm(self, t_s: np.float64, t_ids: np.ndarray) -> np.ndarray:
         """ Observes the current rotor speed setpoint of the turbine.
 
         Args:
             t_s (np.float64): Current simulation time in seconds.
+            t_ids (np.ndarray): Array of turbine IDs.
 
         Raises:
             NotImplementedError: Abstract Method, must be implemented in derived classes.
 
         Returns:
-            np.float64: Current rotor speed setpoint of the turbine (rpm).
+            np.ndarray: Current rotor speed setpoint(s) of the turbine(s) (rpm).
         """
-        return self.obs_rotor_speed_setpoint_radps(t_s) * 60 / (2 * np.pi)
+        return self.obs_rotor_speed_setpoint_radps(t_s, t_ids) * 60 / (2 * np.pi)
 
     @compatibility(CompatibilityLevel.NONE)   
-    def obs_curtailment_factor(self, t_s: np.float64) -> np.float64:
+    def obs_curtailment_factor(self, t_s: np.float64, t_ids: np.ndarray) -> np.ndarray:
         """ Observes the current curtailment factor of the turbine. 0 means no curtailment, 1 means full curtailment.
 
         Args:
             t_s (np.float64): Current simulation time in seconds.
+            t_ids (np.ndarray): Array of turbine IDs.
 
         Raises:
             NotImplementedError: Abstract Method, must be implemented in derived classes.
 
         Returns:
-            np.float64: Current curtailment factor of the turbine (dimensionless).
+            np.ndarray: Current curtailment factor(s) of the turbine(s) (dimensionless).
         """
         raise NotImplementedError
     
     @compatibility(CompatibilityLevel.NONE)   
-    def obs_control_mode(self, t_s: np.float64) -> str:
+    def obs_control_mode(self, t_s: np.float64, t_ids: np.ndarray) -> np.ndarray:
         """ Observes the current control mode of the turbine.
 
         Args:
@@ -163,6 +174,6 @@ class TurbineController(OFFModule):
             NotImplementedError: Abstract Method, must be implemented in derived classes.
 
         Returns:
-            str: Current control mode of the turbine.
+            str: Current control mode of the turbine(s).
         """
         raise NotImplementedError
